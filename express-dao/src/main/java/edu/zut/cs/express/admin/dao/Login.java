3ba56f27d2dd3576af1015be09a39864b0f73a6e
Bircheoods <951793132@qq.com>
2018-05-15 16:29:31 +0800

    更新代码

---
+++ express-dao/src/main/java/edu/zut/cs/express/admin/dao/Login.java
@@ -0,0 +1,10 @@
+ package  edu.zut.cs.express.admin.dao;
+import java.util.*;
+public class Login {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ express-dao/src/test/java/edu/zut/cs/express/admin/dao/MangerTest.java
@@ -0,0 +1,10 @@
+package edu.zut.cs.express.admin.dao;
+
+public class MangerTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
