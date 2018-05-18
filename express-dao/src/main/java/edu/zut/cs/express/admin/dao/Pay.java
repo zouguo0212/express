6860524440f31b567cffcbae9964da79f0e5d61a
g18638141292 <18638141292@163.com>
2018-05-18 14:29:39 +0800

    pay

---
+++ express-dao/src/main/java/edu/zut/cs/express/admin/dao/Pay.java
@@ -0,0 +1,9 @@
+
+package edu.zut.cs.express.admin.dao;
+
+import edu.zut.cs.express.base.dao.GenericDao;
+import edu.zut.cs.express.customer.domain.Customer;
+
+public interface Pay extends GenericDao<Customer,Long>{
+
+}
