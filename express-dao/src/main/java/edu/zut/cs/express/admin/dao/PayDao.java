8fca5a1f2ee6633948c56238b69641e3408b5d81
zouguo0212 <18790058116@163.com>
2018-05-31 11:05:40 +0800

    delete

--- express-dao/src/main/java/edu/zut/cs/express/admin/dao/PayDao.java
+++
@@ -1,9 +0,0 @@
-package edu.zut.cs.express.admin.dao;
-
-import edu.zut.cs.express.admin.domain.Pay;
-import edu.zut.cs.express.base.dao.GenericDao;
-
-public interface PayDao extends GenericDao<Pay, Long>{
-
-
-}
