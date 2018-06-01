a9c61ac6678022273045a103173a6966ff945b43
Modxue <735582586@qq.com>
2018-06-01 08:54:49 +0800

    Journal

--- express-dao/src/main/java/edu/zut/cs/express/manager/domain/Journal.java
+++ express-dao/src/main/java/edu/zut/cs/express/manager/domain/Journal.java
@@ -2,14 +2,11 @@ package edu.zut.cs.express.manager.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import edu.zut.cs.express.base.domain.BaseEntity;
-import edu.zut.cs.express.manager.domain.Group;
 
-@Table(name = "T_ADMIN_Journal")
+@Table(name = "T_Journal")
 @Entity
 public class Journal extends BaseEntity{
 	/**
@@ -28,18 +25,6 @@ public class Journal extends BaseEntity{
 	@Column(name = "OPERATE")
     /**操作*/
     String operate;
-	
-	public Group getGroup() {
-		return group;
-	}
-
-	public void setGroup(Group group) {
-		this.group = group;
-	}
-	
-	@ManyToOne
-	@JoinColumn(name = "GROUP_ID")
-	Group group;
 	
 	public String getLogId() {
 		return logId;
---
+++ express-service/src/main/java/edu/zut/cs/express/admin/service/JournalManager.java
@@ -0,0 +1,11 @@
+package edu.zut.cs.express.admin.service;
+
+import java.util.List;
+
+import edu.zut.cs.express.base.service.GenericManager;
+import edu.zut.cs.express.manager.domain.Journal;
+
+public interface JournalManager extends GenericManager<Journal, Long> {
+	
+	List<Journal> FindAll(String userId);
+}
