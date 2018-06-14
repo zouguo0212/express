964b92ff5a1afebf96d7de889d3978b60da4243f
Modxue <735582586@qq.com>
2018-06-14 11:25:25 +0800

    Journalcontroller

--- express-web/src/main/java/edu/zut/cs/express/manager/web/spring/controller/Journalcontroller.java
+++ express-web/src/main/java/edu/zut/cs/express/manager/web/spring/controller/Journalcontroller.java
@@ -3,16 +3,24 @@ package edu.zut.cs.express.manager.web.spring.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import edu.zut.cs.express.manager.domain.Journal;
 import edu.zut.cs.express.manager.service.JournalManager;
 import edu.zut.cs.express.base.web.spring.controller.GenericController;
 
 @Controller
-@RequestMapping("/Journal")
+@RequestMapping("/manager/Journal")
 public class Journalcontroller extends GenericController<Journal, Long, JournalManager>{
 	
 	JournalManager journalManager;
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/index.html")
+	public String index() {
+		String result = "/Journal/index";
+		return result;
+	}
+
 	
 	@Autowired
 	public void setJournalManager(JournalManager journalManager) {
--- express-web/src/main/webapp/WEB-INF/pages/main/index.jsp
+++ express-web/src/main/webapp/WEB-INF/pages/main/index.jsp
@@ -4,11 +4,37 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Main Page</title>
+<title>Main</title>
+<!-- 引入样式 -->
+<link rel="stylesheet"
+	href="https://unpkg.com/element-ui/lib/theme-chalk/index.css">
+
 </head>
 <body>
 
-    This my main Page。
+	This is my main page!
+
+	<div id="app">
+		<el-button @click="visible = true">Button</el-button>
+		<el-dialog :visible.sync="visible" title="Hello world">
+		<p>Try Element</p>
+		</el-dialog>
+	</div>
+
 
 </body>
+<!-- import Vue before Element -->
+<script src="https://unpkg.com/vue/dist/vue.js"></script>
+<!-- import JavaScript -->
+<script src="https://unpkg.com/element-ui/lib/index.js"></script>
+<script>
+	new Vue({
+		el : '#app',
+		data : function() {
+			return {
+				visible : false
+			}
+		}
+	})
+</script>
 </html>
