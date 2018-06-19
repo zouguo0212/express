557a7a6d8dde650513fa408a381e44f404f4bd87
GreyYa <2667579717@qq.com>
2018-06-19 14:53:33 +0800

    更新

--- express-service/src/main/java/edu/zut/cs/express/journal/service/UJournalManager.java
+++ express-service/src/main/java/edu/zut/cs/express/journal/service/UJournalManager.java
@@ -7,7 +7,35 @@ import edu.zut.cs.express.base.service.GenericManager;
 
 public interface UJournalManager extends GenericManager<UJournal,Long>{
 	
+	/**
+	
+	 * @Description:按用户名查找
+	
+	 * @param username
+	 * @return
+	
+	 * List<UJournal>
+	
+	 * @author: GreyYa
+	
+	 * @time:2018年6月19日 上午10:04:57
+	
+	 */
 	List<UJournal> findByusername(String username);
 	
+	/**
+	
+	 * @Description:按邮箱查找
+	
+	 * @param email
+	 * @return
+	
+	 * List<UJournal>
+	
+	 * @author: GreyYa
+	
+	 * @time:2018年6月19日 上午10:08:02
+	
+	 */
 	List<UJournal> findByemail(String email);
 }
